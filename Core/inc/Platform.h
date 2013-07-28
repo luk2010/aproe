fd8f64afc2e8a0b37c42a973013b20c74dc5a5a0
Luk2010 <jacques.tronconi@gmail.com>
2013-07-28 11:39:57 -1000

    Added Platform class to recognize dynamicly the platform and better specifications.

--- Core/inc/Platform.h
+++ Core/inc/Platform.h
@@ -23,4 +23,42 @@
 #   include "Exception.h"
 #endif
 
+class Platform 
+{
+public:
+    
+    enum 
+    {
+        Unknown     = 0,
+        Windows     = APRO_WINDOWS,
+        WindowsCE   = APRO_WINDOWSCE,
+        Linux       = APRO_LINUX,
+        Apple       = APRO_OSX
+    };
+    
+    static const int get() 
+    {
+#if APRO_PLATFORM == APRO_WINDOWS
+        return (int) Windows;
+#elif APRO_PLATFORM == APRO_WINDOWSCE
+        return (int) WindowsCE;
+#elif APRO_PLATFORM == APRO_LINUX
+        return (int) Linux;
+#elif APRO_PLATFORM == APRO_OSX
+        return (int) Apple;
+#endif
+       return Unknown;
+    }
+    
+    static const char* toString(const int& platform)
+    {
+        if(platform == Unknown) return "Unknown";
+        if(platform == Windows) return "Windows";
+        if(platform == WindowsCE) return "WindowsCE";
+        if(platform == Linux) return "Linux";
+        if(platform == Apple) return "Apple";
+        return "Bad platform id !";
+    }
+};
+
 #endif
